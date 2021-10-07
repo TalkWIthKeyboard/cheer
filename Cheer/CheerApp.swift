07b1a78355a89994aa0dd1b890267dde54e9c89c
Songv <610347922@qq.com>
2021-10-07 16:43:15 +0800

    Initial Commit

---
+++ Cheer/CheerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheerApp.swift
+//  Cheer
+//
+//  Created by 宋伟 on 2021/10/7.
+//
+
+import SwiftUI
+
+@main
+struct CheerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
